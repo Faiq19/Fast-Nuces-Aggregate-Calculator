06c0d8dc04c0adabbfba0b2978568ab89029d0af
Faiq19 <f223270@cfd.nu.edu.pk>
2024-07-13 05:22:22 +0500

    Getting ready for heroku deployment

--- app.py
+++ app.py
@@ -51,4 +51,7 @@ def calculate_admission_test_marks(admission_test_data):
     return total_marks
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if __name__ == '__main__':
+        import os
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port)
